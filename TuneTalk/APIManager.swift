fd9bba5615be48e659eaa29fc1ef8dabae76f60c
jdidat <jdidat@purdue.edu>
2018-05-12 15:56:41 -0400

    first commit

---
+++ TuneTalk/APIManager.swift
@@ -0,0 +1,9 @@
+//
+//  APIManager.swift
+//  TuneTalk
+//
+//  Created by Jackson Didat on 5/12/18.
+//  Copyright © 2018 jdidat. All rights reserved.
+//
+
+import Foundation
---
+++ TuneTalk/ArtistCell.swift
@@ -0,0 +1,24 @@
+//
+//  ArtistCell.swift
+//  TuneTalk
+//
+//  Created by Jackson Didat on 5/12/18.
+//  Copyright © 2018 jdidat. All rights reserved.
+//
+
+import UIKit
+
+class ArtistCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
