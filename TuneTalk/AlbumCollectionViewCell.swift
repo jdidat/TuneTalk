fd922d9e1c2b5ac08f6927c26278ea5c1f91add5
jdidat <jdidat@purdue.edu>
2018-06-06 13:32:10 -0400

    successfully set up collection view with albums

---
+++ TuneTalk/AlbumCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  AlbumCollectionViewCell.swift
+//  TuneTalk
+//
+//  Created by Jackson Didat on 6/6/18.
+//  Copyright © 2018 jdidat. All rights reserved.
+//
+
+import UIKit
+
+class AlbumCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var albumCover: UIImageView!
+    
+}
--- TuneTalk/SimilarArtistsViewController.swift
+++ TuneTalk/SimilarArtistsViewController.swift
@@ -9,7 +9,26 @@
 import UIKit
 import AlamofireImage
 
-class SimilarArtistsViewController: UIViewController {
+class SimilarArtistsViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var albumCollectionView: UICollectionView!
+    
+    var albums: [[String : Any]] = []
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return albums.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AlbumCell", for: indexPath) as! AlbumCollectionViewCell
+        let album = albums[indexPath.item]
+        let albumImages = album["image"] as! [[String : Any]]
+        let albumImage = albumImages[2]["#text"] as! String
+        print(albumImage)
+        let albumURL = URL(string: albumImage)
+        cell.albumCover.af_setImage(withURL: albumURL!)
+        return cell
+    }
     
     var name = ""
     var artist: [[String : Any]] = []
@@ -21,9 +40,14 @@ class SimilarArtistsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        albumCollectionView.dataSource = self
         artistBio!.layer.borderWidth = 1
         artistBio!.layer.borderColor = UIColor.black.cgColor
         searchArtist()
+        getArtistsAlbums(success: { (data) in
+            self.albums = data
+        }, failure: { (error) in })
+        print(albums)
     }
 
     override func didReceiveMemoryWarning() {
@@ -54,7 +78,26 @@ class SimilarArtistsViewController: UIViewController {
         }
         task.resume()
     }
-
+    
+    @objc func getArtistsAlbums(success: @escaping ([[String: Any]]) -> (), failure: @escaping (Error) -> ()) {
+        let url = URL(string: "http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=" + self.name +  "&api_key=e1523431ee9c18604fb535cf31cdbcc8&format=json")!
+        let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with: request) {
+            (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            else if let data = data {
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as! [String : Any]
+                let data = dataDictionary["topalbums"] as! [String : Any]
+                let albums = data["album"] as! [[String : Any]]
+                success(albums)
+                self.albumCollectionView.reloadData()
+            }
+        }
+        task.resume()
+    }
     /*
     // MARK: - Navigation
 
